3ac1a35b1b706c5448bd68528564d7968baac3bd
Alice Alsford <alice.alsford@gmail.com>
2019-11-07 12:31:33 +1300

    boilerplate for react

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+
+    <div>
+      <h1>This is the bookem page</h1>
+    </div>
+
+
+  )
+}
+
+
+export default App
