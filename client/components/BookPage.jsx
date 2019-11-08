6e151f82f2a61416e98d594b491e93f85e77106a
Alice Alsford <alice.alsford@gmail.com>
2019-11-08 18:38:41 +1300

    book form

--- client/components/BookPage.jsx
+++ client/components/BookPage.jsx
@@ -16,8 +16,20 @@ class BookPage extends React.Component {
             <React.Fragment >
                 <form>
                     <label>Date:
-                        <input type="text" name="day" value={this.state.date} />
+                        <input type="date" name="bookingDay" value={this.state.date} required />
                     </label>
+                    <label>Time:
+                        <input type="time" name="bookingTime" required />
+                    </label>
+                    <label>Book:<select name="job_id" id="jobId" >
+                        <option value="1111" name="{{service1}}">Lawn mowing</option>
+                        <option value="1112" name="{{service2}}">Weeding</option>
+                        <option value="1113" name="{{service3}}">Lawn mowing and weeding</option>
+                        <option value="1114" name="{{service4}}">Planting</option>
+                        <option value="1115" name="{{service5}}">Retaining wall</option>
+                    </select>
+                    </label>
+                    <button>Book</button>
                 </form>
 
             </React.Fragment >
@@ -26,3 +38,5 @@ class BookPage extends React.Component {
 }
 
 export default BookPage
+
+
