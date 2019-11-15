1344cbf291c30c4f9112ca23465d6d1da09f7666
Alice Alsford <alice.alsford@gmail.com>
2019-11-15 21:16:21 +1300

    fixed form issue

--- client/components/BookingsPage.jsx
+++ client/components/BookingsPage.jsx
@@ -50,7 +50,7 @@ class BookingsPage extends React.Component {
                         if (booking.bookingDate < today) {
                             return null
                         }
-                        return < li > Date: {booking.bookingDate.split("-").reverse().join('-')}<br /> Time: {booking.bookingTime}<br /> Details: {booking.servicesDescription}<br /><br />
+                        return < li > Date: {moment(booking.bookingDate).format('dddd MMM Do YYYY')}<br /> Time: {booking.bookingTime}<br /> Details: {booking.servicesDescription}<br /><br />
                             <button name={booking.bookingId} onClick={this.deleteBooking}>Cancel</button><br />
                             <hr /></li>
                     })}
