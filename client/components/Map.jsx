ce1e3baaf9cb9b033021cf50a97fefad1b065eb1
Alice Alsford <alice.alsford@gmail.com>
2019-11-20 21:47:02 +1300

    set up for deploy

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,54 +1,54 @@
-import React from 'react'
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+// import React from 'react'
+// import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
 
 
 
-class Map extends React.Component {
-    constructor(props) {
-        super(props)
+// class Map extends React.Component {
+//     constructor(props) {
+//         super(props)
 
-    }
+//     }
 
-    componentDidMount() {
-        console.log(this.props.coords)
-    }
+//     componentDidMount() {
+//         console.log(this.props.coords)
+//     }
 
-    render() {
+//     render() {
 
-        let firstAddress = this.props.coords
-        console.log(firstAddress)
-
-
-        return (
+//         let firstAddress = this.props.coords
+//         console.log(firstAddress)
 
 
-            <LeafletMap
-                center={[-41.1115568, 174.8891837]} //make this usercity
-                zoom={13}
-                maxZoom={20}
-                attributionControl={true}
-                zoomControl={true}
-                doubleClickZoom={true}
-                scrollWheelZoom={true}
-                dragging={true}
-                animate={true}
-                easeLinearity={0.35}
-            >
+//         return (
 
-                <TileLayer
-                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                />
-                <Marker position={[-41.1115568, 174.8891837]}>
-                    <Popup>
-                        Lawn Mowing
-                        09:00
-                        {/* {this.props.description} */}
-                    </Popup>
-                </Marker>
-            </LeafletMap>
-        );
-    }
-}
 
-export default Map
+//             <LeafletMap
+//                 center={[-41.1115568, 174.8891837]} //make this usercity
+//                 zoom={13}
+//                 maxZoom={20}
+//                 attributionControl={true}
+//                 zoomControl={true}
+//                 doubleClickZoom={true}
+//                 scrollWheelZoom={true}
+//                 dragging={true}
+//                 animate={true}
+//                 easeLinearity={0.35}
+//             >
+
+//                 <TileLayer
+//                     url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+//                 />
+//                 <Marker position={[-41.1115568, 174.8891837]}>
+//                     <Popup>
+//                         Lawn Mowing
+//                         09:00
+//                         {/* {this.props.description} */}
+//                     </Popup>
+//                 </Marker>
+//             </LeafletMap>
+//         );
+//     }
+// }
+
+// export default Map
